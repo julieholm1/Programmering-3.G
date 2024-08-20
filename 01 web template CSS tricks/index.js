8648c73e21e09e6098a339bc85e4f926549df741
JulieHolm1 <Julieholm-laursen@outlook.dk>
2024-08-20 09:28:05 +0200

    Oprettet index.html i roden

--- 01 web template CSS tricks/index.js
+++ 01 web template CSS tricks/index.js
@@ -1,5 +1,6 @@
-let currentPage = 1
-let pages = []//array med alle elementer med class = page
+let currentPage = 3
+let pages //array med alle elementer med class = page
+let colors = ['red','orange','yellow','lightgreen','green','darkgreen','lightskyblue','blue','darkblue','purple','pink']
 
 function setup(){
     console.log('P5.js er loaded')
@@ -9,6 +10,14 @@ function setup(){
     pages = selectAll('.page')
     //Pages er nu blevet til en liste med alle class = page ting
     //console.log(pages.length)
+
+    //Lav en masse div'er vi kommer ind i page3
+    for(c of colors){
+        //console.log(c)
+        let div = createDiv()
+        div.style('background-color', c)
+        select('#page3').child(div)
+    }
 }
 
 function shiftPage(num){
